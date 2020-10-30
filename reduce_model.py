4951b89b07a5df8be6278dc799f4743a0b77543e
Amine Abdaoui <abdaoui@lirmm.fr>
2020-10-31 00:49:35 +0100

    fix(reduce_model): model is args.output_model

--- reduce_model.py
+++ reduce_model.py
@@ -119,7 +119,7 @@ def main():
     # convert to tensorflow
     if (args.convert_to_tf):
         if os.path.isfile(f"{args.output_model}/pytorch_model.bin"):
-            print(f"{MODEL_PATH}/tf_model.h5 already exists")
+            print(f"{args.output_model}/tf_model.h5 already exists")
         else:
             tf_model = TFAutoModel.from_pretrained(args.output_model, from_pt=True)
             tf_model.save_pretrained(args.output_model)
